1e99ccfbd1125aa35c3d045dd389507540831562
Jake <jakegeo@sudomail.com>
2022-05-01 08:45:06 -0700

    exercise-01 completed

---
+++ practical-exercise-01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php 
+
+    /* Step 1: Use the Echo Function to say hello with html h1 tags embedded inside php.
+
+       Step 2: Write a comment above the echo function and explain what that function is doing. 
+    */
+    
+    // This function is displaying 'Hello' to the page.
+    echo "<h1>Hello</h1>";
+
+?>
+
+
+</body>
+</html>

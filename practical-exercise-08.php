5a7eb222a11fb005e80a191cb5fbcae2328a283a
Jake <jakegeo@sudomail.com>
2022-05-03 17:39:34 -0700

    steps updated

--- practical-exercise-08.php
+++ practical-exercise-08.php
@@ -1,5 +1,10 @@
 <?php 
 
+/* Step 1: Make a variable with some text as value
+   Step 2: Use crypt() function to encrypt it
+   Step 3: Assign the crypt result to a variable
+   Step 4: Echo the variable
+*/
 
 $password = "mysecretpassword";
 $salt = "iusesomecrazystrings22";
